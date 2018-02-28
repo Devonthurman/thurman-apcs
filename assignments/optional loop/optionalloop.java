ca4a5d7f1091b4748285fbd3df6a84faf7871a1e
Devon Thurman <Thurm.Devo2019@ssusd.org>
2018-02-28 07:47:05 -0800

    base-n exercise done

--- assignments/optional loop/optionalloop.java
+++ assignments/optional loop/optionalloop.java
@@ -1,26 +1,8 @@
 class optionalloop{
     public static void main (String[] args){
-        int n = 0;
-       
-        while (n<3,true){
-            Scanner scan2 = new Scanner(System.in);
-            System.out.print("Enter a Int: ");
-            if(isValidInt(scan2)) {
-                System.out.println("Int Success!");
-            }
-            else {
-                System.out.println("Int Failure!");
-                n++;
-            }
-        }   
-    }
-    public static Boolean isValidInt(Scanner scan2) {
-        try {
-            int s = scan2.nextInt();
+        while (n<3){
+
         }
-        catch(Exception e) {
-            return false;
-        }
-        return true; 
     }
-}
+}       
+     
