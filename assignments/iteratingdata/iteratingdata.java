ba5e7987423c69ce9e7641a90ae8ff6b5ea95f43
devonthurman <devonthurman@hotmail.com>
2018-03-15 18:36:06 -0700

    started

--- assignments/iteratingdata/iteratingdata.java
+++ assignments/iteratingdata/iteratingdata.java
@@ -9,16 +9,17 @@ class iteratingdata{
         // for(int i=1;i<nums.length;i++){
         //     System.out.println(nums[i]);
         // }
-        System.out.println("fibonacci");
-        int[] fibonacci=new int [41];
-        fibonacci[0]=0;
-        fibonacci[1]=1;
-        System.out.println(fibonacci[0]);
-        System.out.println(fibonacci[1]);
-        for(int i=2;i<fibonacci.length;i++){
-            fibonacci[i]=fibonacci[i-2]+fibonacci[i-1];
-            int n=1;
-            System.out.println((n++)+". "+fibonacci[i]);
-        }
+        // System.out.println("fibonacci");
+        // int[] fibonacci=new int [41];
+        // fibonacci[0]=0;
+        // fibonacci[1]=1;
+        // System.out.println(fibonacci[0]);
+        // System.out.println(fibonacci[1]);
+        // for(int i=2;i<fibonacci.length;i++){
+        //     fibonacci[i]=fibonacci[i-2]+fibonacci[i-1];
+        //     int n=1;
+        //     System.out.println((n++)+". "+fibonacci[i]);
+        // }
+        
     }
 }
--- assignments/lists/list.java
+++ assignments/lists/list.java
@@ -1,39 +1,62 @@
 import java.util.*;
-import java.util.Scanner;
 class list{
+    public static ArrayList<String> todolist = new ArrayList<String>(); 
     public static int indexnum=-1;
+    public static String todoitem="-1";
     public static void main(String[] args){
-        ArrayList<String> todolist = new ArrayList<String>(); 
         for(int i=0;i<todolist.size();i++){
             System.out.println(todolist.get(i));
         }
-        viewindex();
-        System.out.println(indexnum);
+        // viewindex();
+        addindex();
+        
     }
-    public static void viewindex(){
-         Scanner scan4 = new Scanner(System.in);
-         while (true) {System.out.print("Would you like to add or delete an index? ");  
-        if(isValidBoolean(scan4)) {
-            break;
+    public static void addindex(){
+        while (true) {
+            System.out.print("Enter ToDo item: ");
+            Scanner scan = new Scanner(System.in);
+            if(scan.hasNextLine()) {
+                todoitem = scan.nextLine();
+                todolist.add(todoitem);
+                 break;
             }
-            else {
-                System.out.println("Invalid input!");
-            } 
         }
+            System.out.println(todolist.get(0));
     }
-    public static Boolean isValidBoolean(Scanner scan4) {
-            String s = scan4.nextLine();
-           return (
-                s.toLowerCase().equals("add")||
-                s.toLowerCase().equals("delete")||
-                s.toLowerCase().equals("+")||
-                s.toLowerCase().equals("-")
-           );
-    }       
 
-    public static Boolean isValidInt(Scanner scan) {
+    // public static void viewindex(){
+    // Scanner scan4 = new Scanner(System.in);
+    //      while (true) {System.out.print("Would you like to add or delete an index? ");  
+    //     if(isValidBoolean(scan4)) {
+    //         break;
+    //         }
+    //         else {
+    //             System.out.println("Invalid input!");
+    //         } 
+    //     }
+    // }
+    // public static Boolean isValidBoolean(Scanner scan4) {
+    //         String s = scan4.nextLine();
+    //        return (
+    //             s.toLowerCase().equals("add")||
+    //             s.toLowerCase().equals("delete")||
+    //             s.toLowerCase().equals("+")||
+    //             s.toLowerCase().equals("-")
+    //        );
+    // }       
+
+    // public static Boolean isValidInt(Scanner scan) {
+    //     try {
+    //         int s = scan2.nextInt();
+    //     }
+    //     catch(Exception e) {
+    //         return false;
+    //     }
+    //     return true; 
+    // }
+    public static Boolean isValidString(Scanner scan) {
         try {
-            int s = scan.nextInt();
+            String s = scan.nextLine();
         }
         catch(Exception e) {
             return false;
