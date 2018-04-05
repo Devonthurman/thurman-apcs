285fc766127d9de14187756bb9cfb0ccb9f42b0c
Devon Thurman <Thurm.Devo2019@ssusd.org>
2018-04-05 07:39:18 -0700

    close

--- assignments/stringdecomp/stringdecomp.java
+++ assignments/stringdecomp/stringdecomp.java
@@ -1,8 +1,76 @@
+import java.util.*;
 class stringdecomp{
+    public static String message="Given the circumstances, I'm not sure if it was a success, but it certainly seems promising.";
+    public static ArrayList<Integer> spaces= new ArrayList<Integer>();
+    public static ArrayList<Integer> punc= new ArrayList<Integer>(); 
+    public static ArrayList<Integer> itidx= new ArrayList<Integer>();
+    public static ArrayList<String> words= new ArrayList<String>();      
+    
     public static void main (String args[]){
-        System.out.println("hello");
+        findpunc();
+        findspace();
+        finditidx();
+        findwords();
+    }
+
+    public static void findpunc(){
+        for(int i=0;i<message.length();i++){
+            if(message.charAt(i)==','){
+                punc.add(i);
+            }
+            if(message.charAt(i)=='.'){
+                punc.add(i);
+            }
+            if(message.charAt(i)=='I'){
+                punc.add(i+1);
+            }
+        }
+        for(int idx : punc) {
+            System.out.println(message.charAt(idx));
+        }
+    }
+
+    public static void findspace(){
+        for(int i=0;i<message.length();i++){
+            if(message.charAt(i)==' '){
+                spaces.add(i);
+            }
+        }
+        for(int idx : spaces) {
+            System.out.println("space");
+        }
+    }
+    public static void finditidx(){
+        for(int i=0;i<message.length();i++){
+            if(message.charAt(i)=='i'){
+                if(message.charAt(i+1)=='t'){
+                    itidx.add(i);
+                }    
+            }
+        }
+        for(int idx : itidx) {
+            System.out.println(message.charAt(idx));
+        }
+    }
+
+     public static void findwords(){
+        for(int i=0;i<message.length();i++){
+            if(message.charAt(i)==' '){
+                if(message.charAt(i-1)=='.'){
+                    words.add("y");
+                }
+                if(message.charAt(i-1)==','){
+                    words.add("t");
+                }
+                else{
+                    words.add("f");
+                } 
+            }
+        }
+        
     }
 }
+
 
 //  String s = "I'd go, but I'm broke.";
 
@@ -28,7 +96,7 @@ class stringdecomp{
 //     System.out.println(c5); prints "."
 
 //     int idx3 = s.indexOf('\'');
-//     System.out.println(idx3);
+//     System.out.println(idx3); prints "1"
 
 //     int idx4 = s.indexOf("go");
 //     System.out.println(idx4); prints "4"
