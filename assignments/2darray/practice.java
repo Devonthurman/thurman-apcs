b5aafc7a2547184d15244b67fe48cf739a6e47b2
Devon Thurman <Thurm.Devo2019@ssusd.org>
2018-04-16 07:38:46 -0700

    need finish

--- assignments/2darray/practice.java
+++ assignments/2darray/practice.java
@@ -3,19 +3,32 @@ class practice{
     public static int[][] strt= new int[3][3];
     public static int[][] revers= new int[3][3];
     public static void main (String[] arg){
-        
+        int num=2;
+        for(num=2;num<6;num++){
+            if(num==4){
+            }
+            else{
+                for(int i=0;i<strt.length;i++){
+                    int num2=1;
+                    for(int j=0;j<strt[i].length;j++){
+                        strt[i][j]=num^num2;
+                        num2++;
+                    }
+                }
+            }
+        }    
     }
 
-    public static int[][] reverse(int[][] strt){
-        int num1=0;
-        for(int i=strt.length-1;i>=0;i--){
-            int num2=0;
-            for(int j=strt[i].length-1;j>=0;j--){
-                revers[i][j]=strt[num1][num2];
-                num2++;
-            }
-            num1++;
-        }
-        return revers;
-    }
+    // public static int[][] reverse(int[][] strt){
+    //     int num1=0;
+    //     for(int i=strt.length-1;i>=0;i--){
+    //         int num2=0;
+    //         for(int j=strt[i].length-1;j>=0;j--){
+    //             revers[i][j]=strt[num1][num2];
+    //             num2++;
+    //         }
+    //         num1++;
+    //     }
+    //     return revers;
+    // }
 }
