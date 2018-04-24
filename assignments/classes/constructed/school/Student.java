9458548dc09b0c8eb02b4761a7a8ac729a415030
Devon Thurman <Thurm.Devo2019@ssusd.org>
2018-04-24 07:28:15 -0700

    finish

--- assignments/classes/constructed/school/Student.java
+++ assignments/classes/constructed/school/Student.java
@@ -1,16 +1,17 @@
 package school;
-
 import java.util.*;
 
 public class Student {
     public String firstName;
     public String lastName;
     public int id;
+    public ArrayList<course> courses;
 
     public Student(String firstName, String lastName, int id) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
+        this.courses= new ArrayList<course>();
     }
 
     public String getfullname(){
@@ -20,5 +21,8 @@ public class Student {
     public String tostring(){
         String studentinfo="Name: "+firstName+" "+lastName+"; ID:"+id;
         return studentinfo;
+    }
+    public void addcourses(course c){
+        courses.add(c);
     }
 }
