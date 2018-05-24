8301236c478e3356f4ed2225bcf108ed0486998e
Devon Thurman <Thurm.Devo2019@ssusd.org>
2018-05-24 07:26:57 -0700

    going

--- projects/Memory/Launcher.java
+++ projects/Memory/Launcher.java
@@ -1,7 +1,7 @@
 import game.*;
 
 public class Launcher {
-    public static void main(String args[]){
-        MemoryGame game= new MemoryGame(4,6,10);
+    public static void main(String[] args){
+        MemoryGame game= new MemoryGame(4,4,10);
     }
 }
